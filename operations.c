b43ae2d8ee6af3dd567d187b0026a23776c1ca44
mlemoula <martin.lemoulant@gmail.com>
2025-02-25 01:50:35 +0100

    fix norminette

--- operations.c
+++ operations.c
@@ -6,20 +6,20 @@
 /*   By: mlemoula <mlemoula@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 23:32:06 by mlemoula          #+#    #+#             */
-/*   Updated: 2025/02/21 00:11:06 by mlemoula         ###   ########.fr       */
+/*   Updated: 2025/02/25 01:46:37 by mlemoula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void sa(t_stack *a);
-void sb(t_stack *b);
-void ss(t_stack *a, t_stack *b);
-void pa(t_stack *a, t_stack *b);
-void pb(t_stack *a, t_stack *b);
-void ra(t_stack *a);
-void rb(t_stack *b);
-void rr(t_stack *a, t_stack *b);
-void rra(t_stack *a);
-void rrb(t_stack *b);
-void rrr(t_stack *a, t_stack *b);
+void	sa(t_stack *a);
+void	sb(t_stack *b);
+void	ss(t_stack *a, t_stack *b);
+void	pa(t_stack *a, t_stack *b);
+void	pb(t_stack *a, t_stack *b);
+void	ra(t_stack *a);
+void	rb(t_stack *b);
+void	rr(t_stack *a, t_stack *b);
+void	rra(t_stack *a);
+void	rrb(t_stack *b);
+void	rrr(t_stack *a, t_stack *b);
--- push_swap.h
+++ push_swap.h
@@ -6,19 +6,21 @@
 /*   By: mlemoula <mlemoula@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 17:00:05 by mlemoula          #+#    #+#             */
-/*   Updated: 2025/02/21 00:10:06 by mlemoula         ###   ########.fr       */
+/*   Updated: 2025/02/25 01:45:41 by mlemoula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-typedef struct		s_node {
+typedef struct s_node
+{
 	int				value;
 	struct s_node	*next;
 }					t_node;
 
-typedef struct {
+typedef struct s_stack
+{
 	t_node	*top;
 }			t_stack;
 
