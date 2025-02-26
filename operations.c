998de42df13b3d0b95011f11cdc470489eac0bb9
mlemoula <martin.lemoulant@gmail.com>
2025-02-27 00:26:49 +0100

    wip

--- operations.c
+++ operations.c
@@ -6,7 +6,7 @@
 /*   By: mlemoula <mlemoula@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 23:32:06 by mlemoula          #+#    #+#             */
-/*   Updated: 2025/02/26 01:23:36 by mlemoula         ###   ########.fr       */
+/*   Updated: 2025/02/27 00:26:14 by mlemoula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,17 +26,61 @@ void	sa(t_list **a)
 	}
 }
 
-void	sb(t_list *b)
+void	sb(t_list **b)
 {
-	
+	t_list	*first;
+	t_list	*second;
+	if (*b && (*b)->next)
+	{
+		first = *b;
+		second = first->next;
+		first->next = second->next;
+		second->next = first;
+		*b = second;
+	}
+}
+
+void	ss(t_list **a, t_list **b)
+{
+	sa(a);
+	sb(b);
+}
+
+void	pa(t_list **a, t_list **b)
+{
+	t_list	*tmp;
+	if (*b)
+	{
+		tmp = (*b)->next;
+		ft_lstadd_front(a, *b);
+		*b = tmp;
+	}
+}
+
+void	pb(t_list **a, t_list **b)
+{
+	t_list	*tmp;
+	if (*a)
+	{
+		tmp = (*a)->next;
+		ft_lstadd_front(b, *a);
+		*a = tmp;
+	}
+}
+
+void	ra(t_list **a)
+{
+	t_list	*tmp;
+	if (*a && (*a)->next)
+	{
+		tmp = (*a)->next;
+		ft_lstadd_front(a, ft_lstlast(a);
+		*a = tmp;
+	}	
 }
 
 
 
-void	ss(t_list *a, t_list *b);
-void	pa(t_list *a, t_list *b);
-void	pb(t_list *a, t_list *b);
-void	ra(t_list *a);
 void	rb(t_list *b);
 void	rr(t_list *a, t_list *b);
 void	rra(t_list *a);
