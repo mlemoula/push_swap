e3050399e0d5f82c65f58ec4d3e686d6024415a8
Martin <martin.lemoulant@gmail.com>
2025-03-07 18:33:48 +0100

    Add files via upload

---
+++ libft_copie/ft_lstlast.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlemoula <mlemoula@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/27 15:38:38 by mlemoula          #+#    #+#             */
+/*   Updated: 2024/11/27 15:44:04 by mlemoula         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Returns the last node of the list.
+*/
+
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	if (!lst)
+		return (NULL);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
+}
---
+++ libft_copie/ft_lstsize.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstsize.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlemoula <mlemoula@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/27 15:17:56 by mlemoula          #+#    #+#             */
+/*   Updated: 2024/11/27 15:38:20 by mlemoula         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Counts the number of nodes in a list.
+lst: The beginning of the list.
+*/
+
+#include "libft.h"
+
+int	ft_lstsize(t_list *lst)
+{
+	int	i;
+
+	if (!lst)
+		return (0);
+	i = 1;
+	while (lst->next)
+	{
+		i++;
+		lst = lst->next;
+	}
+	return (i);
+}
